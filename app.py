e7e8fc442ca4b1dbdca1e9d0469262876fea904d
PARTH_DS <parthvashishtha498@gmail.com>
2026-07-28 11:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ This system predicts whether the tumor is **Benign** or **Malignant**.
 
 """)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
         server_port=int(os.environ.get("PORT", 7860))
